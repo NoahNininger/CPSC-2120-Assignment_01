e2666ef048e6113b46ec0dcfddea838f8464a9eb
Noah Nininger <114418869+NoahNininger@users.noreply.github.com>
2024-01-16 11:06:20 -0500

    Create review.h

---
+++ Review_04/review.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+
+#include <vector>
+
+
+
+
+int add(int a, int b);
+void ReadStdIn();
+int WriteOut(std::string output);
+int WriteOut(int output);
+int WriteOut(double output);
+std::vector <int> InitializeArray(int size);
+void ReadStdIn2();
+void ReadWrite();
+void LoopThrough(double * data, int size);
+int Fibonacci(int a);
